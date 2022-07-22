463b126ac889cb7b9966dc048f4d29f00a970d07
MartinPek <martin_pek@web.de>
2022-07-22 10:56:29 +0200

    updated skeleton

--- src/m_Telephone.ino
+++ src/m_Telephone.ino
@@ -2,6 +2,7 @@
 
 #include <stb_common.h>
 #include <stb_brain.h>
+#include <avr/wdt.h>
 
 STB STB;
 STB_BRAIN BRAIN;
@@ -9,7 +10,12 @@ STB_BRAIN BRAIN;
 
 void setup() {
     
+    // starts serial and default oled
     STB.begin();
+    
+    Serial.println("WDT endabled");
+    wdt_enable(WDTO_8S);
+    
     STB.i2cScanner();
     
     STB.rs485SetSlaveAddr(0);
@@ -25,5 +31,8 @@ void setup() {
 
 
 void loop() {
+    STB.rs485AddToBuffer("my response/data");
+    // sends out buffer once polled along with !eof, to handle cmds be aware this sends out the !EOF
     STB.rs485SlaveRespond();
 }
+
